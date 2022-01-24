e140874bc21bce42ca3cf5eb674f82bef4138bbf
ulfijulianti <ulfijulianti07@gmail.com>
2022-01-24 11:34:38 +0700

    Laravel Maul

---
+++ resources/views/maul.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Beranda</title>
+    <link rel="stylesheet" href="{{url('/css/style.css')}}">
+    <script src="{{url('/javascript/maul.js')}}"></script>
+</head>
+<body>
+    <h1>Selamat Datang, Muhamad Maulana Ramdani</h1>
+</body>
+</html>
